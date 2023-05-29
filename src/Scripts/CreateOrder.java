065f35d57e09533675489c554094b3bcaceb4ae5
natarajtestgithub <134951632+natarajtestgithub@users.noreply.github.com>
2023-05-29 23:14:39 +0530

    Update CreateOrder.java

--- src/Scripts/CreateOrder.java
+++ src/Scripts/CreateOrder.java
@@ -1,9 +1,9 @@
 package Scripts;
 
 import Pages.BaseClass;
-import Pages.Context;
 import Pages.LandingPage;
 import java.util.ArrayList;
+import javax.naming.Context;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.*;
@@ -12,7 +12,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
@@ -23,6 +22,9 @@ public class CreateOrder extends BaseClass{
 	WebDriver driver;
 	LandingPage lp1;
 	ArrayList<Context> context;
+	ExtentReports extent;
+	ExtentTest test;
+	
 	
 	
 	@BeforeMethod
@@ -30,25 +32,39 @@ public class CreateOrder extends BaseClass{
 		
 	driver=invokeBrowser();
     lp1= PageFactory.initElements(driver, LandingPage.class);
-    //generating the reports in the format of extent report 
-	ExtentReports extent = new ExtentReports();
-	ExtentSparkReporter htmlReporter = new ExtentSparkReporter("C:\\repo\\Capegemni\\CapegemniDemo\\test-output\\Reports.html");
-	extent.attachReporter(htmlReporter);
-    ExtentTest test = extent.createTest("My Test", "Description of my test");
-    extenttest.set(test);
+    
+       // Setting up Extent Reports
+ 		extent = new ExtentReports();
+ 		ExtentSparkReporter htmlReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "/test-output/Extent_Reports.html");
+ 		extent.attachReporter(htmlReporter);
+ 		test = extent.createTest("Create Order Test", "Verifying order creation");
+ 		extenttest.set(test);
+
     	}
 	
 	@Test
-	public void firstTest() throws InterruptedException {
-	
-	  lp1.register();
-	  BaseClass.logging("succesfully navigated to the registered page");  
-      lp1.creatingNewAccount();
-      BaseClass.logging("succesfully Registered into the page");
-      lp1.addingtoCart();
-      BaseClass.logging("succesfully added item into the cart");
+	public void createOrderTest() throws InterruptedException {
+		// Register Test Step
+		lp1.register();
+		BaseClass.logging("Successfully navigated to the registered page");
+		test.log(Status.PASS, "Successfully navigated to the registered page");
+		
+		// Creating New Account Test Step
+		lp1.creatingNewAccount();
+		BaseClass.logging("Successfully registered into the page");
+		test.log(Status.PASS, "Successfully registered into the page");
+		
+		// Adding to Cart Test Step
+	   lp1.addingtoCart();
+		BaseClass.logging("verifying an item added into the cart");
+		test.log(Status.FAIL, "Just to check in report - Failed to add item into the cart");
+		
+		//confirm order
+		lp1.confirmOrder();
+		BaseClass.logging("Successfully placed an order");
+		test.log(Status.PASS, "succesfully places an order");
 	}
-
+	
 	@AfterMethod
 	public void closingBrowser() {
 		
